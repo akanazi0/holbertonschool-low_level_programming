dda7c02c2ca679d37b538c5cf9e9715c7305ee42
akanazi0 <abdulelah.khma@gmail.com>
2026-07-10 06:06:03 +0300

    "Add task 1 last digit program"

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,15 +1,35 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	int last_d = n % 10;
+
+	printf("Last digit of %d is %d ", n, last_d);
+
+	if (last_d > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (last_d == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
 	return (0);
 }
 
