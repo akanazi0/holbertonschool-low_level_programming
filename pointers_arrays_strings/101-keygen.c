fe396f1607d0b96a11ae9a4309dc5a905a281b81
akanazi0 <abdulelah.khma@gmail.com>
2026-07-18 20:57:12 +0300

    feat: add keygen program to generate random valid passwords

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for 101-crackme
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sum = 2772;
+	int rand_char;
+
+	srand(time(NULL));
+
+	while (sum > 126)
+	{
+		rand_char = (rand() % 94) + 32;
+		putchar(rand_char);
+		sum -= rand_char;
+	}
+
+	putchar(sum);
+
+	return (0);
+}
