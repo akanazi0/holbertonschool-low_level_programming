b0ae1e223fcdd91f38631934c1ebfbd6fdd69cb4
akanazi0 <abdulelah.khma@gmail.com>
2026-07-18 21:32:07 +0300

    feat: add _strncat function and corresponding test files

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string to append to
+ * @src: The source string to be appended
+ * @n: The maximum number of bytes to use from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	dest[dest_len] = '\0';
+
+	return (dest);
+}
