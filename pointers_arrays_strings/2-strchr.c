74ecd08cb4c2b29bf8c97e657d916b33618a0892
akanazi0 <abdulelah.khma@gmail.com>
2026-07-18 22:49:10 +0300

    feat: implement _strchr function to locate a character in a string and update main header

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to be searched
+ * @c: The character to locate
+ *
+ * Return: A pointer to the first occurrence of c, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s >= '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (*s == '\0')
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
