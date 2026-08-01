10e1a2278104a67d8f0c7145fdfc6fa29a4441e2
akanazi0 <abdulelah.khma@gmail.com>
2026-08-01 15:24:12 +0300

    feat: add initial implementation of argc and argv functionality

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
